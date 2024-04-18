dc1bb42a4ebe5d033064adc5ac8e6d958c2c7184
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-04-18 14:23:03 -0400

    functions

--- Example#2.py
+++ Example#2.py
@@ -1,12 +1,21 @@
 #-------------------------------------------------------------------------
 # Name:		    Lesson 5.1 Intro to Functions & Parameters
 # Purpose:		Example #2
-# Author:		  LastName. FirstInitial
-# Created:		dd/mm/yyyy
+# Author:		Bassias. B
+# Created:		18/04/2024
 #-------------------------------------------------------------------------
 
 # Example #1:
 ## Part (a) Create a function that will check if a number is even or odd
 
+def even_or_odd(num = int(input("Enter a number: "))):
+    if num == 0:
+        print("Zero is neither even nor odd.")
+    elif (num % 2) == 0:
+        print("Your number is even.")
+    else:
+        print("Your number is odd.")
 
 ## Part (b) Use your function by calling it
+
+even_or_odd()
